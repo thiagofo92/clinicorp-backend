de7fed17cd0c38d92ef9261755f7d74a1933cfa1
TFO <thiagofo92@gmail.com>
2024-06-11 19:25:19 -0300

    :label: add dto to communicate with repository

---
+++ backend/src/dto/project.dto.mjs
@@ -0,0 +1,19 @@
+/**
+ * @typedef ProjectCreateDto
+ * @type {Object}
+ * @property {string} id
+ * @property {string} name
+ * @property {string} userRootId
+ * @property {Array<string>} taskId
+ * @property {string} description
+ * @property {Date} createdAt
+ * */
+
+/**
+ * @typedef ProjectUpdateDto
+ * @type {Object}
+ * @property {string} id
+ * @property {string} [name]
+ * @property {Array<string>} [ taskId ]
+ * @property {string} [ description ]
+ * */
---
+++ backend/src/dto/user.dto.mjs
@@ -0,0 +1,16 @@
+/**
+ * @typedef UserCreateDto
+ * @type {Object}
+ * @property {string} name
+ * @property {string} login
+ * @property {string} pass
+ * */
+
+/**
+ * @typedef UserUpdateDto
+ * @type {Object}
+ * @property {string} id
+ * @property {string} [ name ]
+ * @property {string} [ login ]
+ * @property {string}[ pass ]
+ * */
